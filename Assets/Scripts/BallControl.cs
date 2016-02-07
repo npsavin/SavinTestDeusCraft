c208bfb12901d7e2d326104ca4c292d07b705787
Savin Nikita <npsavin@rambler.ru>
2016-02-07 22:25:37 +0600

    First Commit

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BallControl : MonoBehaviour
+    {
+
+
+        private bool _ballIsActive;
+        private Vector3 _ballPosition;
+        private Vector3 _ballInitialForce;
+
+        // GameObject
+        public GameObject PlayerObject;
+        private Rigidbody _rb;
+
+        // используйте этот метод для инициализации
+        private void Start()
+        {
+
+            _rb = GetComponent<Rigidbody>();
+            // создаем силу
+            _ballInitialForce = new Vector3(300.0f, 0, 0);
+
+            // переводим в неактивное состояние
+            _ballIsActive = false;
+
+            // запоминаем положение
+            _ballPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            // проверка нажатия на пробел
+            if (Input.GetButtonDown("Jump"))
+            {
+                // проверка состояния
+                if (!_ballIsActive)
+                {
+                    // сброс всех сил
+                    _rb.isKinematic = false;
+                    _rb.AddForce(_ballInitialForce);
+                    // зададим активное состояние
+                    _ballIsActive = true;
+                }
+
+                if (!_ballIsActive && PlayerObject != null)
+                {
+                    // задаем новую позицию шарика
+                    _ballPosition.x = PlayerObject.transform.position.x;
+
+                    // устанавливаем позицию шара
+                    transform.position = _ballPosition;
+                }
+                
+                // проверка падения шара
+                if (_ballIsActive && transform.position.y < -6)
+                {
+                    _ballIsActive = !_ballIsActive;
+                    _ballPosition.x = PlayerObject.transform.position.x;
+                    _ballPosition.y = -4.2f;
+                    transform.position = _ballPosition;
+
+                    _rb.isKinematic = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/BatControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BatControl : MonoBehaviour {
+        public float PlayerVelocity;
+        private Vector3 _playerPosition;
+        private Vector2 _mousePosition;
+
+        // Use this for initialization
+        void Start () {
+            _playerPosition = gameObject.transform.position;
+            _mousePosition = Input.mousePosition;
+
+
+        }
+	
+        // Update is called once per frame
+        void Update () {
+            // горизонтальное движение
+
+            _playerPosition.z += ChangePosition(Input.mousePosition) * PlayerVelocity;
+            _mousePosition = Input.mousePosition;
+
+            // выход из игры
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+            // обновим позицию платформы
+            transform.position = _playerPosition;
+        }
+
+        private int ChangePosition(Vector2 position)
+        {
+            if (_mousePosition.x < position.x)
+            {
+                return -1;
+            }
+            if(_mousePosition.x > position.x)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
